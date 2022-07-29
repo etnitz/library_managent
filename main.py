35b62c8dbb425ebb5a7c26fddda714a4efa6322a
Ethan Nitz <etnitz@gmail.com>
2022-07-29 13:17:55 -0500

    finished main code

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ class Library:
         else:
             print('The book does not exist in the book lending database')
 
-library = Library()
+library = Library(booksList=[], name='Judy')
 
 def main():
     while True:
@@ -48,10 +48,26 @@ def main():
         
         print(choice)
 
-        userInput = input('Press Q to quit or C to continue').lower()
+        userInput = input('Press C to continue or Q to quit').lower()
         if userInput == 'c':
             userChoice = int(input('Select an option to continue:'))
             if userChoice == 1:
                 library.displayBooks()
             elif userChoice == 2:
-                
+                book = input('Enter the name of the book you want to lend:')
+                user = input('Enter the name of the user:')
+                library.lendBook(book, user)
+            elif userChoice == 3:
+                book = input('Enter the name of the book you want to add:')
+                library.addBook(book)
+            elif userChoice == 4:
+                book = input('Enter the name of the book you want to return:')
+                library.returnBook(book)
+            else:
+                print('Please choose a valid option: 1 to display books, 2 to lend a book, 3 to add a book, 4 to return a book.')
+        elif userInput == 'q':
+            break
+        else:
+            print('Please choose a valid option: C to continue or Q to quit.')
+
+main()
