1eacc48710a1c827a76bab5485f4c0d8e0705233
Ethan Nitz <etnitz@gmail.com>
2022-07-29 12:46:06 -0500

    created lend book func

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 class Library:
     
-    def __init__(self, booklist, name):
-        self.booklist = booklist
+    def __init__(self, booksList, name):
+        self.booksList = booksList
         self.name = name
         self.lendDict = {}
 
     def displayBooks(self):
         print(f'We have the following books in our library: {self.name}')
-        for book in self.booklist:
+        for book in self.booksList:
             print(book)
 
     def addBook(self, book):
-        pass
+        if book in self.booksList:
+            print('Book already exists')
+        else:
+            self.booksList.append(book)
+            print('Book added')
 
-    def lendBook():
-        pass
+    def lendBook(self, book, user):
+        if book in self.booksList:
+            if book not in self.lendDict.keys():
+                self.lendDict.update({book: user})
+                print('Book has been lended. Database updated')
+            else:
+                print(f'Book is already being used by {self.lendDict[book]}')
+        else:
+            print("Apologies, We currently do not have this book in our library")
 
     def returnBook():
         pass
