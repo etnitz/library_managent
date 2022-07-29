0698e8fcc3ddbf25b688ddce09b91f8f7933fea7
Ethan Nitz <etnitz@gmail.com>
2022-07-29 12:30:51 -0500

    started Library class

---
+++ main.py
@@ -0,0 +1,18 @@
+class Library:
+    
+    def __init__(self, booklist, name):
+        self.booklist = booklist
+        self.name = name
+        self.lendDict = {}
+
+    def displayBooks():
+        pass
+
+    def addBook():
+        pass
+
+    def lendBook():
+        pass
+
+    def returnBook():
+        pass
